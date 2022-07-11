10f7a04abafa808d3b336c967fff25158a119051
Jason Sautieres <jason.sautieres@gmail.com>
2022-07-11 23:36:04 +0200

    feat: implement the API

---
+++ server/db.js
@@ -0,0 +1,21 @@
+const url = 'mongodb://localhost:27017'; // Connection URL
+const dbName = 'GoTO'; // Database Name
+
+const getAccueil = (db, callback) => {
+  return new Promise((resolve, reject) => {
+    db.collection('accueil')
+      .find()
+      .toArray((err, docs) => {
+        if (err) {
+          return reject(err); // Reject the Promise with an error
+        }
+        return resolve(docs); // Resolve (or fulfill) the promise with data
+      });
+  });
+};
+
+module.exports = {
+  getAccueil,
+  url,
+  dbName,
+};
---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const dbFunctions = require('./db');
+const MongoClient = require('mongodb').MongoClient;
+
+var db = null;
+
+// REQUESTS
+app.get('/accueil', (req, res) => {
+  if (db != null) {
+    dbFunctions
+      .getAccueil(db, () => {})
+      .then(
+        data => res.status(200).json({ data }),
+        err => res.status(404).json({ erreur: err }),
+      );
+  } else {
+    res.status(404).json({ erreur: 'erreur' });
+  }
+});
+
+// Connexion
+MongoClient.connect(dbFunctions.url, { useNewUrlParser: true })
+  .then(client => {
+    console.log('Connected successfully to server');
+    db = client.db(dbFunctions.dbName);
+    app.listen(8080, () => {
+      console.log("Serveur à l'écoute");
+    });
+  })
+  .catch(error => console.error(error));
