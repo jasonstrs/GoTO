d208155916b30a1bf63f25577d2daf8e270cc4fc
Jason Sautieres <jason.sautieres@gmail.com>
2022-07-18 16:12:37 +0200

    add redux library

---
+++ redux/features/user/userSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    token: null,
+  },
+  reducers: {
+    setToken: (state, action) => {
+      state.token = action.payload.token;
+    },
+  },
+});
+
+export const { setToken } = userSlice.actions;
+
+export default userSlice.reducer;
